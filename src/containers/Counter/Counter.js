11863cb58a9dc55ea6cbbf9e4525b2745ebdbeff
Uthaeus <romanlavery@gmail.com>
2018-09-09 11:47:11 -0600

    Action creators and async code

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement, add, subtract, storeResult, deleteResult } from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -57,13 +57,13 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch(increment()),
-        onDecrementCounter: () => dispatch(decrement()),
-        onAddCounter: () => dispatch(add(10)),
-        onSubtractCounter: () => dispatch(subtract(15)),
-        onStoreResult: (result) => dispatch(storeResult()),
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onAddCounter: () => dispatch(actionCreators.add(10)),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
         //({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch(deleteResult())
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
         //({type: actionTypes.DELETE_RESULT, resultElId: id})
     };
 };
